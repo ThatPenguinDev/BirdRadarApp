159471759dabcc97e621a48582dc0c6db7352df6
Daiyaan786 <st10082008@vcconnect.edu.za>
2023-11-13 18:14:33 +0200

    commit daiyaan

--- app/src/main/java/com/example/loginpage/MainActivity.kt
+++ app/src/main/java/com/example/loginpage/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.loginpage
 
 
-import com.google.maps.android.PolyUtil
+
 import android.content.Context
 import android.os.Bundle
 import android.view.GestureDetector
@@ -15,6 +15,11 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.loginpage.databinding.ActivityMainBinding
 import android.content.Intent
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -22,11 +27,13 @@ class MainActivity : AppCompatActivity() {
     lateinit var password: EditText
     lateinit var loginButton: Button
     lateinit var signUpButton: Button // Add this line for the sign-up button
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
 
         // Initialize the views
         username = binding.email
@@ -34,6 +41,7 @@ class MainActivity : AppCompatActivity() {
         loginButton = binding.loginButton
         signUpButton = binding.signUpButton // Initialize the sign-up button
 
+        val auth = FirebaseAuth.getInstance()
         // Set up a gesture detector to detect swipe down
         val gestureDetector = GestureDetector(this, object : SimpleOnGestureListener() {
             override fun onDown(e: MotionEvent): Boolean {
@@ -62,16 +70,40 @@ class MainActivity : AppCompatActivity() {
             gestureDetector.onTouchEvent(event)
         }
 
-        loginButton.setOnClickListener(View.OnClickListener {
-            if (username.text.toString() == "user" && password.text.toString() == "1234") {
-                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
+        loginButton.setOnClickListener {
+            val enteredUsername = username.text.toString()
+            val enteredPassword = password.text.toString()
 
-                val intent = Intent(this, MainActivity3::class.java)
-                startActivity(intent)
-            } else {
-                Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
-            }
-        })
+            val databaseReference = FirebaseDatabase.getInstance().getReference("users")
+            databaseReference.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    var loginSuccessful = false
+
+                    for (userSnapshot in dataSnapshot.children) {
+                        val user = userSnapshot.getValue(User::class.java)
+
+                        if (user != null && user.email == enteredUsername && user.password == enteredPassword) {
+                            // Login Successful
+                            loginSuccessful = true
+                            break
+                        }
+                    }
+
+                    if (loginSuccessful) {
+                        Toast.makeText(this@MainActivity, "Login Successful!", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this@MainActivity, MainActivity3::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this@MainActivity, "Login Failed!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    // Handle any database error
+                    Toast.makeText(this@MainActivity, "Database Error: ${databaseError.message}", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
 
         signUpButton.setOnClickListener(View.OnClickListener {
             // Handle sign-up button click
@@ -79,8 +111,8 @@ class MainActivity : AppCompatActivity() {
             // Add your sign-up logic here
 
             // Open SignUpActivity
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
+            val intent1 = Intent(this, MainActivity2::class.java)
+            startActivity(intent1)
         })
     }
 
--- app/src/main/java/com/example/loginpage/MainActivity2.kt
+++ app/src/main/java/com/example/loginpage/MainActivity2.kt
@@ -4,18 +4,27 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.GestureDetector
 import android.view.MotionEvent
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.loginpage.databinding.ActivityMain2Binding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+
 
 class MainActivity2 : AppCompatActivity() {
 
     private lateinit var binding: ActivityMain2Binding
-
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.email
+        binding.password
+        binding.confirmPassword
+
+        auth = FirebaseAuth.getInstance()
         // Set up a gesture detector
         val gestureDetector = GestureDetector(this, object : GestureDetector.SimpleOnGestureListener() {
             override fun onFling(
@@ -37,16 +46,64 @@ class MainActivity2 : AppCompatActivity() {
         // Set up the onTouchListener for the root view
         binding.root.setOnTouchListener { _, event ->
             gestureDetector.onTouchEvent(event)
+
+        }
+        fun saveUserDataToDatabase(userId: String?, email: String, password: String) {
+            if (userId != null) {
+                val databaseReference = FirebaseDatabase.getInstance().getReference("users")
+                val user = User(userId, email ,password) // Add other properties as needed
+                databaseReference.child(userId).setValue(user)
+            }
         }
 
         // Set up click listener for SignUp button
         binding.signUpButton.setOnClickListener {
-            // Handle sign-up button click
-            // Add your sign-up logic here
+            val emailInput = binding.email.text.toString()
+            val passwordInput = binding.password.text.toString()
+            val confirmPasswordInput = binding.confirmPassword.text.toString()
 
-            // Open MainActivity3
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            if (emailInput.isEmpty() || passwordInput.isEmpty() || confirmPasswordInput.isEmpty()) {
+                // Handle empty fields error
+                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (passwordInput != confirmPasswordInput) {
+                // Handle password mismatch error
+                Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            auth.createUserWithEmailAndPassword(emailInput, passwordInput)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign-up success
+                        val user = auth.currentUser
+
+                        // Save additional user data to Firebase Realtime Database
+                        saveUserDataToDatabase(user?.uid, emailInput , passwordInput)
+
+                        Toast.makeText(this, "Sign Up Successful", Toast.LENGTH_SHORT).show()
+                        // You can do additional actions here, like navigating to the next activity.
+                        val intent = Intent(this, MainActivity3::class.java)
+                        startActivity(intent)
+                    } else {
+                        // Sign-up failed
+                        // You can handle errors here.
+                        Toast.makeText(this, "Sign Up Failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
         }
+
+        // Function to save user data to Firebase Realtime Database
+
+
+
+
+
+
     }
 }
+
+
